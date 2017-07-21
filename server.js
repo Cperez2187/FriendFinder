2e1e2c021b90b471248ee5b2b8c988847425e132
cperez2187 <cperez2187@gmail.com>
2017-07-21 16:45:06 -0700

    Continous updates

--- server.js
+++ server.js
@@ -3,3 +3,28 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Create list of friends to match to
+let friendsList = [];
+
+// Set up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: "application/vnd.api+json"
+}));
+
+// Routes to all the html files
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.listen(PORT, () => {
+    console.log('App listening on PORT ' + PORT);
+});
+
+
